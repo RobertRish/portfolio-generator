071b87b2a8018f5ef88785a4b8644b78618d5700
Robby Rish <robbyrish13@gmail.com>
2022-02-07 02:46:56 -0500

    added inquirer module

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+/* const fs = require('fs');
 const generatePage = require('./src/page-template');
-
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
 
 const pageHTML = generatePage(name, github);
 
@@ -15,4 +8,14 @@ fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+}); */
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
